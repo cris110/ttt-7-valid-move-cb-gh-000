2758354f1f1d735ef486c20637f4cb25c34cbe08
Cristiano Moreira <cristiano.moreira01@fatec.sp.gov.br>
2019-08-19 00:57:18 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,21 @@
 # code your #valid_move? method here
 
-
+def valid_move?(board, input)
+  input -= 1
+  if input.between(1, 9) == false
+    puts "Invalid move"
+  elsif position_taken(board, input) == true
+    puts "Invalid move"
+  else
+    true
+  end
+end
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+
+def position_taken?(board, index)
+  if board[index] == "" || board[index] == " " || board[index] == nil
+    false
+  else
+    true
+  end
+end
